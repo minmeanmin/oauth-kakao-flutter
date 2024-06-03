8c5f7cb740b9540bd344438d81ca81a150fc98af
minmeanmin <wed1604@naver.com>
2024-06-03 14:21:09 +0900

    kakao login 성공

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:oauthapp/_core/move.dart';
 import 'ui/login_page/login_page.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // runApp() 호출 전 Flutter SDK 초기화
+  KakaoSdk.init(nativeAppKey: '51954f8d3d73cbf57597140dbcbee7f6');
   runApp(ProviderScope(child: MyApp()));
 }
 
