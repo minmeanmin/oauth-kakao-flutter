2f05d49b1ff0fccbe5ab64698fd0381c0fb1db85
minmeanmin <wed1604@naver.com>
2024-06-04 16:23:53 +0900

    Naver login 성공

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
 import 'package:oauthapp/_core/move.dart';
 import 'ui/login_page/login_page.dart';
+import 'package:flutter_naver_login/flutter_naver_login.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
   // runApp() 호출 전 Flutter SDK 초기화
-  KakaoSdk.init(nativeAppKey: '51954f8d3d73cbf57597140dbcbee7f6');
+  FlutterNaverLogin.initSdk(clientId: 'R20LPQpzlu2cKacLhpow', clientName: 'login_naver_flutter', clientSecret: '3A3QssAyQc');
   runApp(ProviderScope(child: MyApp()));
 }
 
